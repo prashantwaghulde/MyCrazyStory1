2cb2f250b51cfb555bad60122c492c470e758e58
Prashant Waghulde <prashant.waghulde@dell.com>
2018-08-29 22:48:16 -0600

    Constraint Layout demo

--- app/src/main/java/com/paw/mycrazystory1/MainActivity.java
+++ app/src/main/java/com/paw/mycrazystory1/MainActivity.java
@@ -1,13 +1,30 @@
 package com.paw.mycrazystory1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextName;
+    private Button buttonStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextName = findViewById(R.id.nameEditText);
+        buttonStart = findViewById(R.id.startButton);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString();
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
