a8e30a9c43b49d3c1ff0545421660cc45c17f416
Prashant Waghulde <prashant.waghulde@dell.com>
2018-09-09 22:38:30 -0600

    Added remaining code to complete the project

--- app/src/main/java/com/paw/mycrazystory1/ui/MainActivity.java
+++ app/src/main/java/com/paw/mycrazystory1/ui/MainActivity.java
@@ -1,11 +1,14 @@
-package com.paw.mycrazystory1;
+package com.paw.mycrazystory1.ui;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.paw.mycrazystory1.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,9 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        editTextName.setText("");
+    }
+
     private void startStory(String name) {
         Intent intent = new Intent(this,StoryActivity.class);
-        intent.putExtra("name",name);
+        Resources resources = getResources();
+        String key = resources.getString(R.string.key_name);
+        intent.putExtra(key,name);
         startActivity(intent);
     }
 }
